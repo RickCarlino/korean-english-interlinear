f2360b0623d7f50810ada5f0305b8bf5ac2e46c4
danjclegg <danjclegg@gmail.com>
2020-11-23 18:52:26 -0800

    various improvements

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # korean-english-interlinear
 Command-line python script that, from supplied Korean text, generates Korean-English interlinear direct translation rendered in html with parts-of-speech tags and definitions.
 
-Requires the following software and python packages to be installed: 
-- [Konlpy (using Hannaanum, Okt, and Mecab-ko)](https://github.com/konlpy/konlpy), 
-- [Soylemma](https://github.com/lovit/korean_lemmatizer), and 
+Requires the following software and python packages to be installed:
+- [Konlpy (using Okt, and Mecab-ko)](https://github.com/konlpy/konlpy),
+- [Soylemma](https://github.com/lovit/korean_lemmatizer), and
 - [KEngDic dictionary using PostgreSQL](https://github.com/garfieldnate/kengdic).
 
 ![Alt text](/screenshot.png?raw=true)
--- korean-english-interlinear.py
+++ korean-english-interlinear.py
@@ -1,24 +1,10 @@
 #!/usr/bin/python
 
-# import sys
-# filename = sys.argv[1]
-
-filename = "/home/ddog/Documents/Korean Learning/korean-english-interlinear/sample-input.txt"
-
-with open(filename) as f:
-    content = f.readlines()
-
-content = [x.strip() for x in content] 
-
-import re
-word_regex = r'\b\w+\b'
-
-structured_content = []
-
+import sys
 import html
+import urllib.parse
+import psycopg2
 
-from konlpy.tag import Hannanum
-hannanum = Hannanum()
 from konlpy.tag import Mecab
 mecab = Mecab()
 from konlpy.tag import Okt
@@ -27,11 +13,53 @@ okt = Okt()
 from soylemma import Lemmatizer
 lemmatizer = Lemmatizer()
 
+##input setup
+# filename = sys.argv[1]
+filename = "/home/ddog/Documents/Korean Learning/korean-english-interlinear/sample-input.txt"
 
-import psycopg2
+with open(filename) as f:
+    content = f.readlines()
+content = [x.strip() for x in content] 
+
+
+##connect to database and check tables are there
 con = psycopg2.connect(database="kenddic", user="ddog", password="", host="127.0.0.1", port="5432")
 print("Database opened successfully")
 cur = con.cursor()
+
+#sql code to create another table like the main one, this taken from KEngDic:
+create_added_table = """CREATE TABLE korean_english_added (
+    id SERIAL PRIMARY KEY,
+    wordid integer,
+    word character varying(130),
+    syn character varying(190),
+    def text,
+    posn integer,
+    pos character varying(13),
+    submitter character varying(25),
+    doe timestamp without time zone,
+    wordsize smallint,
+    hanja character varying,
+    wordid2 integer,
+    extradata character varying
+);"""
+
+cur.execute("select * from information_schema.tables where table_name=%s", ('korean_english',))
+if not bool(cur.rowcount):
+    print("Error, database dictionary table missing or not set up.")
+    sys.exit(1)
+
+cur.execute("select * from information_schema.tables where table_name=%s", ('korean_english_added',))
+if not bool(cur.rowcount):
+    cur.execute(create_added_table)
+    cur.close()
+    con.commit()
+    cur = con.cursor()
+    cur.execute("select * from information_schema.tables where table_name=%s", ('korean_english_added',))
+    if not bool(cur.rowcount):
+        print("Error, database added dictionary table missing or not set up.")
+        sys.exit(1)
+
 
 ##mecab-ko used by konlpy seems to be using Sejong tagset as seen:
 #http://semanticweb.kaist.ac.kr/nlp2rdf/resource/sejong.owl
@@ -95,30 +123,22 @@ SejongTagset = {
     'SY':   ('OtherSymbol',     'Symbol'), 
     'NR':   ('Numeral',         'Symbol'), 
     'NNBC': ('DependantNoun',   'Noun'), 
-
-    #for double coded ones using first coded tag as above
-    # 'VCP+ETM':  ('Copula',          'Verb'), 
-    # 'VCP+EP':   ('Copula',          'Verb'), 
-    # 'VV+EC':    ('VerbalPredicate', 'Verb'), 
-    # 'VA+EP':    ('Adjective',       'Verb'),
-    # 'VV+EP':    ('Adjective',       'Verb') 
-    
 }
 
 #https://praacticalaac.org/praactical/how-we-do-it-using-language-boards-to-support-aac-use-by-nerissa-hall-and-hillary-jellison/
 #https://alltogether.wordpress.com/2007/09/17/goossens-crain-elder-communication-overlay-color-reminder/
 #https://vimawesome.com/plugin/solarized-8
 SuperClass_Colours = {
-    'Adverb': '#b78a00',
-    'Noun': 'hsl(0, 0%, 20%)',
-    'Pronoun': '#25a399',
-    'Verb': '#cc4a0e',
+    'Adverb':       '#b78a00',
+    'Noun':         'hsl(0, 0%, 20%)',
+    'Pronoun':      '#25a399',
+    'Verb':         '#cc4a0e',
     'CardinalNumber': 'hsl(0, 0%, 20%)',
-    'Determiner': '#729f01',
-    'ForeignWord': 'hsl(0, 0%, 70%)',
+    'Determiner':   '#729f01',
+    'ForeignWord':  'hsl(0, 0%, 70%)',
     'Interjection': '#cc4a0e',
-    'Symbol': 'hsl(0, 0, 20%)',
-    'Particle': '#657b85'
+    'Symbol':       'hsl(0, 0, 20%)',
+    'Particle':     '#657b85'
 }
 
 def get_sejongtagset_name(tag):
@@ -142,7 +162,6 @@ def get_sejongtagset_colour(tag):
         return ""
     return SuperClass_Colours[sejong_superclass]
     
-    
 def print_tree(tree, prepend = ""):
     #print("call on: " + str(tree))
     print(prepend + "[")
@@ -157,14 +176,24 @@ def print_tree(tree, prepend = ""):
     print(prepend + "]")    
 
 def get_trans_new_fetch(word, addition = ""):
-    cur.execute("SELECT word, TRIM(coalesce(def, '')) AS trans FROM korean_english WHERE word like %s;", (word + addition,))        
+    if addition == "":
+        comparator = "="
+    else:
+        comparator = "LIKE"
+    cur.execute("SELECT word, TRIM(coalesce(def, '')) AS trans FROM korean_english WHERE word " + comparator + " %s UNION SELECT word, TRIM(coalesce(def, '')) AS trans FROM korean_english_added WHERE word " + comparator + " %s;", (word + addition, word + addition))
     rows = cur.fetchall()
     if type(rows) is list and len(rows) > 0:
+        if addition == "":
+            for row in rows:
+                if row is not None:
+                    if row[1] == "":
+                        print("Note, blank dictionary definition for word: " + row[0])
+                        blank_dictionary_definitions.append(row)
         return "<br>\n".join(["<br>".join([row[0],row[1]]) for row in rows if row is not None])
     return ""
     
-def get_trans_new(word):
-    print("Getting translation: " + word)
+def get_trans_new_recursive(word):
+    #print("Last ditch searching for translation for: " + word)
     if word == "":
         return ""
     translation = get_trans_new_fetch(word, "")
@@ -176,21 +205,65 @@ def get_trans_new(word):
     translation = get_trans_new_fetch(word, "%")
     if translation != "":
         return translation
-    return get_trans_new(word[:-1])
+    return get_trans_new_recursive(word[:-1])
+
+def get_translation(plain_word):
+    if plain_word == "":
+        return ""
+
+    #first look up the given word in dictionary
+    translation = get_trans_new_fetch(plain_word)
+
+    #then try lemmatizing the word one way (returns one), see if in dic
+    if translation == "":
+        lemmatized_word = okt.pos(plain_word, norm=True, stem=True)
+        if type(lemmatized_word) == list and len(lemmatized_word) == 0:
+            print("warning, zero length list back from okt for: " + plain_word)
+        elif type(lemmatized_word) == list and type(lemmatized_word[0]) == tuple:
+            lemmatized_word = lemmatized_word[0][0]
+            translation = get_trans_new_fetch(lemmatized_word)
+
+    #then try backup lemmatizing method (returns multiple), see if in dic
+    if translation == "":
+        lemmatized_words = lemmatizer.lemmatize(plain_word)
+        for lemmatized_word in lemmatized_words:
+            if type(lemmatized_word) == tuple:
+                lemmatized_word = lemmatized_word[0]
+                translation = get_trans_new_fetch(lemmatized_word)
+                if translation != "": 
+                    break
+                    
+    #then try backup method of extending and truncating recursively
+    if translation == "":
+            translation = get_trans_new_recursive(plain_word)
+            if translation != "":
+                translation = "(?)" + translation
+                print("Note, no match for word: " + plain_word + ",\n using questionable match: " + translation.replace("<br>\n", "; ").replace("<br>"," ")[:20] + "...")
+                no_translations.append((plain_word, ""))
+
+    if translation == "":
+        print("Error, no non-empty dictionary entry for word: " + plain_word)
+        no_translations.append((plain_word, ""))
+
+    return translation
+
 
 ## Main construction
+structured_content = []
+
+#to catch warnings to deal with
+blank_dictionary_definitions = []
+no_translations = []
+
 for j in range(len(content)):
     line = content[j]
     if line != "":
         if line[0] == "#":
             structured_content.append(line[1:])
         else:
-            #see for explan but not how I installed (installed through konlpy) https://pypi.org/project/python-mecab-ko/
-            #another possible avenue: https://lovit.github.io/nlp/2018/06/07/lemmatizer/
+            #see for explanation, but not how I installed this (installed through konlpy):
+            #https://pypi.org/project/python-mecab-ko/
             structured_content.append(mecab.pos(line, flatten = False))
-
-#print("structured_content:")
-#print(structured_content)
 
 def format_tree(branch, d = 0):
     #print("call on: " + str(tree))
@@ -209,104 +282,69 @@ def format_tree(branch, d = 0):
                 xprint('    </li>')
         elif type(branch[0]) == tuple:
             #we have a list of tuples comprising a word
-            #branch = [('“', 'SSO'), ('톱질', 'NNG'), ('하', 'XSV'), ('세', 'EC')]
-
-            plain_word = "".join([(x[0] if (x[1][0] != "S") else "") for x in branch])
-            
-            if plain_word == "":
-                print("warning, plain word is empty, printing branch: ")
-                print(branch)
-            
-            #print("working on plain word: " + plain_word)
-
-            full_word = "".join([
-                                    (
-                                        "<span style='color:" + get_sejongtagset_colour(x[1]) + ";'>" + 
-                                        html.escape(x[0]) +
-                                        "</span>"
-                                        
-                                    ) for x in branch
-                                ])
-            
-            link = "https://en.dict.naver.com/#/search?query=" + urllib.parse.quote(plain_word)
-            
-            non_symbol_pos = [
-                                (x[0] + " " + x[1]) for x in branch if x[1][0] != "S"
-                            ]
-            pos_info = "<br>".join(non_symbol_pos)
-            pos_info_long = "<br>".join([
-                                            (
-                                                x[0] + " " + get_sejongtagset_name(x[1])
-                                            ) for x in branch if x[1][0] != "S"
-                                        ])
-            
-            #first look up the given word in dictionary
-            translation = get_trans_new_fetch(plain_word)
-            #if translation != "":
-            #    print("plain lookup in dic found match for: " + plain_word)
-
-            #then try lemmatizing the word one way (returns one), see if in dic
-            if translation == "":
-                lemmatized_word = okt.pos(plain_word, norm=True, stem=True)
-                if type(lemmatized_word) == list and len(lemmatized_word) == 0:
-                    print("warning, zero length list back from okt for: " + plain_word)
-                elif type(lemmatized_word) == list and type(lemmatized_word[0]) == tuple:
-                    lemmatized_word = lemmatized_word[0][0]
-                    translation = get_trans_new_fetch(lemmatized_word)
-                    #if translation != "":
-                    #    print("used main okt lemmatizer and found a match in dic for: " + lemmatized_word)
-
-            #then try backup lemmatizing method (returns multiple), see if in dic
-            if translation == "":
-                lemmatized_words = lemmatizer.lemmatize(plain_word)
-                for lemmatized_word in lemmatized_words:
-                    if type(lemmatized_word) == tuple:
-                        lemmatized_word = lemmatized_word[0]
-                        translation = get_trans_new_fetch(lemmatized_word)
-                        if translation != "": 
-                            print("used backup soylemma lemmatizer and found a match in dic for: " + lemmatized_word)
-                            break
-
-            descriptiontop = html.escape(pos_info).replace("&lt;br&gt;","<br>")
-            tooltiptop = html.escape(pos_info_long).replace("&lt;br&gt;","<br>")
-
-            tshort_split = translation.split("<br>")
-            if len(tshort_split) >= 2:
-                tshort = "<br>".join([tshort_split[0][:10],tshort_split[1][:8]])
-            else: 
-                tshort = ("<br>".join(tshort_split))[:10]
-            
-            descriptionbot = html.escape(tshort).replace("&lt;br&gt;","<br>")
-            tooltipbot = html.escape(translation).replace("&lt;br&gt;","<br>")
-
-
-            xprint('    <li>')
-            xprint('      <ol class=word>')
-            xprint(f'        <li lang=es><a class=diclink target="_blank" rel="noopener noreferrer" href="{link}">{full_word}</a></li>')
-            xprint(f'        <li lang=en_MORPH class=tooltip>{descriptiontop}<span class=tooltiptext>{tooltiptop}</span></li><br>')
-            xprint(f'        <li lang=en_MORPH class=tooltip>{descriptionbot}<span class=tooltiptext>{tooltipbot}</span></li>')
-
-            xprint('      </ol>')
-            xprint('    </li>')
-            
+            format_branch(branch)
         else:
             print("error, unexpected branch type" + str(type(branch)))
     else: 
         print("error, expected list got something else")
 
 
-            
+def format_branch(branch):
+    #example format:
+    #branch = [('“', 'SSO'), ('톱질', 'NNG'), ('하', 'XSV'), ('세', 'EC')]
+
+    plain_word = "".join([(x[0] if (x[1][0] != "S") else "") for x in branch])
+    if plain_word == "":
+        if sum([1 for x in branch if x[1][0] != "S"]) > 0:
+            print("Warning, plain word is empty, printing branch: ")
+            print(branch)
+
+    full_word = "".join([   (   "<span style='color:" + get_sejongtagset_colour(x[1]) + ";'>" + 
+                                html.escape(x[0]) +
+                                "</span>"
+                            ) for x in branch   ])
+                            
+    link = "https://en.dict.naver.com/#/search?query=" + urllib.parse.quote(plain_word)
+    
+    non_symbol_pos = [ (x[0] + " " + x[1]) for x in branch if x[1][0] != "S" ]
+    
+    pos_info = "<br>".join(non_symbol_pos)
+    pos_info_long = "<br>".join([   (
+                                        x[0] + " " + get_sejongtagset_name(x[1])
+                                    ) for x in branch if x[1][0] != "S"  ])
+                                    
+    translation = get_translation(plain_word)
+
+    descriptiontop = html.escape(pos_info).replace("&lt;br&gt;","<br>")
+    tooltiptop = html.escape(pos_info_long).replace("&lt;br&gt;","<br>")
+
+    tshort_split = translation.split("<br>")
+    if len(tshort_split) >= 2:
+        tshort = "<br>".join([tshort_split[0][:10],tshort_split[1][:8]])
+    else: 
+        tshort = ("<br>".join(tshort_split))[:10]
+    
+    descriptionbot = html.escape(tshort).replace("&lt;br&gt;","<br>")
+    tooltipbot = html.escape(translation).replace("&lt;br&gt;","<br>")
+
+    xprint('    <li>')
+    xprint('      <ol class=word>')
+    xprint(f'        <li lang=es><a class=diclink target="_blank" rel="noopener noreferrer" href="{link}">{full_word}</a></li>')
+    xprint(f'        <li lang=en_MORPH class=tooltip>{descriptiontop}<span class=tooltiptext>{tooltiptop}</span></li><br>')
+    xprint(f'        <li lang=en_MORPH class=tooltip>{descriptionbot}<span class=tooltiptext>{tooltipbot}</span></li>')
+    xprint('      </ol>')
+    xprint('    </li>')
+    
+##main proceedures for generating output
 output = ""
 
 def xprint(message):
     global output
     output = output + message + "\n"
 
-import urllib.parse
 
 for j in range(len(structured_content)):
     line = structured_content[j]
-
     if type(line) is str:
         xprint('<div class=wrapper>')
         xprint('    <ol class=sentence>')
@@ -322,8 +360,25 @@ for j in range(len(structured_content)):
     elif type(line) is list:
         format_tree(line)        
 
+##Enter missing words to database
+missing_words = blank_dictionary_definitions + no_translations
+if len(missing_words) > 0:
+    if(input("\nWould you like to insert " + str(len(missing_words)) + " missing word definitions into database for next run? (y/n):") == "y"):
+        submitter = input("Enter your name: ")
+        import time    
+        for entry in missing_words:
+            translation = input("For word " + entry[0] + " enter translation, f to finish, or leave blank for unkown:")
+            if translation == "f": 
+                break
+            if translation != "":
+                doe = time.strftime('%Y-%m-%d %H:%M:%S')
+                cur.execute("INSERT INTO korean_english_added (word, def, submitter, doe) VALUES (%s, %s, %s, %s);", (entry[0], translation, submitter, doe))
+        con.commit()
 
 con.close()
+
+
+##print and format output
 
 header = """<!DOCTYPE html>
 <html>
@@ -331,14 +386,13 @@ header = """<!DOCTYPE html>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!--  http://jsfiddle.net/snifty/Ty8Vp/2/-->
-	<!-- https://linguistics.stackexchange.com/questions/3/how-do-i-format-an-interlinear-gloss-for-html -->
-	<!-- https://stackabuse.com/text-translation-with-google-translate-api-in-python/ -->
-	<!-- http://lostintranslationland.blogspot.com/2011/05/heungbu-and-nolbu.html?m=1 -->
-    <!-- https://www.w3schools.com/css/css_tooltip.asp -->
+<!--  http://jsfiddle.net/snifty/Ty8Vp/2/-->
+<!-- https://linguistics.stackexchange.com/questions/3/how-do-i-format-an-interlinear-gloss-for-html -->
+<!-- https://stackabuse.com/text-translation-with-google-translate-api-in-python/ -->
+<!-- http://lostintranslationland.blogspot.com/2011/05/heungbu-and-nolbu.html?m=1 -->
+<!-- https://www.w3schools.com/css/css_tooltip.asp -->
 
 <style>
-
 ol,ul{
     list-style:none;
 }
@@ -351,12 +405,10 @@ body,div,li,ol,p,pre,ul{
 body {
     background-color: white; 
     font-size: 16px;
-}
-
-body{
     font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
     padding:0;
     margin:0;
+    background-color: #fdf6e4; font-size: 16px;
 }
 
 a{
@@ -366,10 +418,6 @@ a{
 
 a:hover{
     text-decoration:underline;
-}
-
-body {
-    background-color: #fdf6e4; font-size: 16px;
 }
 
 div.wrapper { 
@@ -388,7 +436,6 @@ ol.sentence li[lang=es]{
    font-weight:bold;  
     font-size: 20px; 
 }
-
 
 ol.sentence li[lang=en_MORPH]{
    font-family: monospace;  
@@ -447,14 +494,13 @@ border: 1px dotted #ccc;
 .tooltip .tooltiptext::after {
   content: " ";
   position: absolute;
-  top: 50%;
+  top: 12px;
   right: 100%; /* To the left of the tooltip */
   margin-top: -5px;
   border-width: 5px;
   border-style: solid;
   border-color: transparent hsla(0, 0%, 80%, 50) transparent transparent;
 }
-
 
 /* Show the tooltip text when you mouse over the tooltip container */
 .tooltip:hover .tooltiptext {
@@ -464,9 +510,6 @@ border: 1px dotted #ccc;
 a.diclink {
     text-decoration: none;
 }
-
-
-
 </style>
 </head>
 <body>
